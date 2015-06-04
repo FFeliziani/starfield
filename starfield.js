44ae06f4601bb93c42962bc5ff9309554240e9da
Emiliano Carlesi <ECARLESI.external@unicredit.eu>
2015-06-04 10:59:54 +0200

    Stars now move away from the center in a straight line towards the border. They still don't do anything AT the border, but we'll see

--- starfield.js
+++ starfield.js
@@ -3,12 +3,15 @@ var STARNUM = 1000;
 var WINDOW_WIDTH = window.outerWidth;
 var WINDOW_HEIGHT = window.outerHeight;
 var STARSIZE = 2;
+var SPEED = 1;
+var RATIO = WINDOW_HEIGHT / WINDOW_WIDTH;
 
 // VAR
 var starList = null;
 var canvas = null;
 var ctx = null;
-var index = 0;
+var drawIndex = 0;
+var updateIndex = 0;
 	
 function init() {
 	// IMPOSTO IL CREATO
@@ -33,6 +36,17 @@ function init() {
 	ctx.fillStyle('rgb(0,0,0)');
 	ctx.fillRect(0, 0, canvas.style.width, canvas.style.height);
 	setTimeout(draw(), 30);
+	setTimeout(update(), 30);
+}
+
+function update() {
+	var star = starList[updateIndex];
+	var center = {x: WINDOW_WIDTH / 2, y: WINDOW_HEIGHT / 2};
+	// ALLONTANO LA STELLA DAL CENTRO
+	if(star.x < center.x) star.x -= SPEED; else star.x += SPEED;
+	if(star.y < center.y) star.y -= SPEED * RATIO; else star.y += SPEED * RATIO;
+	updateIndex += 1;
+	if (updateIndex == 1000) updateIndex = 0;
 }
 
 function draw()
@@ -40,9 +54,9 @@ function draw()
 	if (canvas.getContext) {
 		ctx = canvas.getContext('2d');
 		ctx.fillStyle('rgb(255,255,255)');
-		var star = starList[index];
+		var star = starList[drawIndex];
 		ctx.fillRect(star.x, star.y, STARSIZE, STARSIZE);
-		index += 1;
-		if (index = 1000) index = 0;
+		drawIndex += 1;
+		if (drawIndex = 1000) drawIndex = 0;
 	}
 }
